347b33697e0158e32a8b8442c825439cf2c614e8
Adarsh <adarshsingh15169@gmail.com>
2025-10-24 00:40:28 +0530

    first commit

---
+++ 1stprogram.py
@@ -0,0 +1,14 @@
+# We are printing the whole poem by using the multi line comment 
+print("""Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are""")
